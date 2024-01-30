49aafb18217faaeb4e72480084c9f254cb16bdd5
Nabiyevich21 <mmedia10020@gmail.com>
2024-01-30 16:28:01 +0500

    navbar finished

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const navbarAnimation = () => {
+  const navList = document.querySelector(".navbar__list");
+  const navBurger = document.querySelector(".navbar__burger");
+  const BurgerOne = document.querySelector(".navbar__burger-one");
+  const BurgerTwo = document.querySelector(".navbar__burger-two");
+  const BurgerThere = document.querySelector(".navbar__burger-there");
+  navBurger.addEventListener("click", () => {
+    navList.classList.toggle("active");
+    if (navList.classList.contains("active")) {
+      navList.style.transform = "translate(0)";
+      navList.style.transition = "transform 0.3s";
+    } else {
+      navList.style.transform = "translate(-100%)";
+    }
+    navBurger.classList.toggle("BurgerActive");
+  });
+};
+navbarAnimation();
